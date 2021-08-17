8a8cd3ea6a546ed0cf9e2a731afcf0bf7ba3d486
tianbing <tianbing@kuaishou.com>
2021-08-17 21:03:21 +0800

    parser

---
+++ ast/ast.go
@@ -0,0 +1,57 @@
+package ast
+
+import (
+	"monkey/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+/**
+如果一个程序由多个源文件组成，怎么确定第一个语句是什么？ 跟main有关吗？
+ */
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+type LetStatement struct {
+	Token token.Token; //'let' token
+	Name * Identifier;
+	Value * Expression;
+}
+
+func (ls *LetStatement) statementNode() {
+}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token;  //token.IDENT;
+	Value string		//为什么不使用token的Literal()，而是使用Value; Literal表示的是此法解析的元素，Value是实际可求值的东西
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
